82035c590e15351d83bfae6ea3cfbaa6c8b8bc7c
Matsuda, Akihit <matsuda104@gmail.com>
2019-06-02 14:43:47 +0900

    implemented to show/hide FFAB in UserActivity

--- app/src/main/java/com/freshdigitable/udonroad2/UserActivity.kt
+++ app/src/main/java/com/freshdigitable/udonroad2/UserActivity.kt
@@ -3,23 +3,32 @@ package com.freshdigitable.udonroad2
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil.setContentView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.viewpager.widget.ViewPager
 import com.freshdigitable.udonroad2.databinding.ActivityUserBinding
 import com.freshdigitable.udonroad2.model.FragmentScope
 import com.freshdigitable.udonroad2.model.ListQuery
 import com.freshdigitable.udonroad2.model.ViewModelKey
+import com.freshdigitable.udonroad2.navigation.FragmentContainerState
+import com.freshdigitable.udonroad2.navigation.Navigation
+import com.freshdigitable.udonroad2.navigation.NavigationDispatcher
+import com.freshdigitable.udonroad2.navigation.NavigationEvent
+import com.freshdigitable.udonroad2.timeline.TimelineEvent
 import com.freshdigitable.udonroad2.timeline.TimelineFragment
 import com.freshdigitable.udonroad2.timeline.TimelineViewModel
 import com.freshdigitable.udonroad2.timeline.TimelineViewModelModule
 import com.google.android.material.appbar.AppBarLayout
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.android.AndroidInjection
 import dagger.android.AndroidInjector
 import dagger.android.ContributesAndroidInjector
@@ -31,6 +40,8 @@ import javax.inject.Inject
 class UserActivity : HasSupportFragmentInjector, AppCompatActivity() {
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
+    @Inject
+    lateinit var navigation: Navigation<UserActivityState>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
@@ -39,7 +50,8 @@ class UserActivity : HasSupportFragmentInjector, AppCompatActivity() {
         binding.lifecycleOwner = this
 
         binding.userPager.adapter = object : FragmentStatePagerAdapter(
-            supportFragmentManager, RESUME_ONLY_CURRENT_FRAGMENT) {
+            supportFragmentManager, RESUME_ONLY_CURRENT_FRAGMENT
+        ) {
             override fun getItem(position: Int): Fragment {
                 return when (position) {
                     0 -> TimelineFragment.newInstance(ListQuery.Timeline(userId))
@@ -68,6 +80,18 @@ class UserActivity : HasSupportFragmentInjector, AppCompatActivity() {
         binding.userAppBar.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { appBar, offset ->
             viewModel.setAppBarScrollRate(Math.abs(offset).toFloat() / appBar.totalScrollRange.toFloat())
         })
+        binding.userPager.addOnPageChangeListener(object : ViewPager.SimpleOnPageChangeListener() {
+            override fun onPageSelected(position: Int) {
+                viewModel.setCurrentPage(position)
+            }
+        })
+        viewModel.fabVisible.observe(this, Observer {
+            if (it == true) {
+                binding.userFab.show()
+            } else {
+                binding.userFab.hide()
+            }
+        })
     }
 
     private val userId: Long
@@ -89,22 +113,59 @@ class UserActivity : HasSupportFragmentInjector, AppCompatActivity() {
     override fun supportFragmentInjector(): AndroidInjector<Fragment> = injector
 }
 
-@Module(includes = [
-    TimelineViewModelModule::class,
-    UserViewModelModule::class
-])
-interface UserActivityModule {
+class UserActivityState : FragmentContainerState
+
+class UserActivityNavigation(
+    navigator: NavigationDispatcher,
+    activity: UserActivity,
+    @IdRes containerId: Int,
+    viewModelFactory: ViewModelProvider.Factory
+) : Navigation<UserActivityState>(navigator, activity, containerId) {
+
+    private val viewModel =
+        ViewModelProviders.of(activity, viewModelFactory).get(UserViewModel::class.java)
+
+    override fun onEvent(event: NavigationEvent): UserActivityState? {
+        if (event is TimelineEvent.TweetItemSelected) {
+            viewModel.setSelectedItemId(event.selectedItemId)
+        }
+        return null
+    }
+
+    override fun navigate(s: UserActivityState?) {}
+}
+
+@Module(
+    includes = [
+        TimelineViewModelModule::class,
+        UserViewModelModule::class
+    ]
+)
+abstract class UserActivityModule {
     @ContributesAndroidInjector
     @FragmentScope
-    fun contributeTimelineFragment(): TimelineFragment
+    abstract fun contributeTimelineFragment(): TimelineFragment
 
     @Binds
     @IntoMap
     @ViewModelKey(TimelineViewModel::class)
-    fun bindTimelineFragment(viewModel: TimelineViewModel): ViewModel
+    abstract fun bindTimelineFragment(viewModel: TimelineViewModel): ViewModel
 
     @Binds
     @IntoMap
     @ViewModelKey(UserViewModel::class)
-    fun bindUserViewModel(viewModel: UserViewModel): ViewModel
+    abstract fun bindUserViewModel(viewModel: UserViewModel): ViewModel
+
+    @Module
+    companion object {
+        @JvmStatic
+        @Provides
+        fun provideUserActivityNavigation(
+            navigator: NavigationDispatcher,
+            activity: UserActivity,
+            viewModelFactory: ViewModelProvider.Factory
+        ): Navigation<UserActivityState> {
+            return UserActivityNavigation(navigator, activity, 0, viewModelFactory)
+        }
+    }
 }
--- app/src/main/java/com/freshdigitable/udonroad2/UserViewModel.kt
+++ app/src/main/java/com/freshdigitable/udonroad2/UserViewModel.kt
@@ -1,12 +1,14 @@
 package com.freshdigitable.udonroad2
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import com.freshdigitable.udonroad2.data.repository.RepositoryComponent
 import com.freshdigitable.udonroad2.data.repository.UserRepository
 import com.freshdigitable.udonroad2.model.User
+import com.freshdigitable.udonroad2.timeline.SelectedItemId
 import dagger.Module
 import dagger.Provides
 
@@ -31,6 +33,33 @@ class UserViewModel(
 
     fun setAppBarScrollRate(rate: Float) {
         appBarScrollRate.value = rate
+    }
+
+    private val currentPage = MutableLiveData<Int>().apply { value = 0 }
+    private val selectedItemId =
+        MutableLiveData<MutableMap<Int, SelectedItemId?>>().apply { value = mutableMapOf() }
+
+    private val _fabVisible = MediatorLiveData<Boolean>().also {
+        it.addSource(currentPage) { updateFabVisible() }
+        it.addSource(selectedItemId) { updateFabVisible() }
+    }
+    val fabVisible: LiveData<Boolean> = _fabVisible
+
+    fun setCurrentPage(index: Int) {
+        this.currentPage.value = index
+    }
+
+    fun setSelectedItemId(selectedItemId: SelectedItemId?) {
+        val map = requireNotNull(this.selectedItemId.value)
+        val curr = requireNotNull(currentPage.value)
+        map[curr] = selectedItemId
+        this.selectedItemId.value = map
+    }
+
+    private fun updateFabVisible() {
+        val map = requireNotNull(this.selectedItemId.value)
+        val curr = requireNotNull(currentPage.value)
+        _fabVisible.value = map[curr] != null
     }
 }
 
