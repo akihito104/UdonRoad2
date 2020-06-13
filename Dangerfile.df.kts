5304226f3a1198e6367f61c180b6c65e39be34d8
Matsuda, Akihit <matsuda104@gmail.com>
2020-06-13 22:51:15 +0900

    added to check lint action

---
+++ Dangerfile.df.kts
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2020. Matsuda, Akihit (akihito104)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@file:Repository("https://repo.maven.apache.org")
+@file:DependsOn("groupId:artifactId:version")
+
+import systems.danger.kotlin.*
+
+danger(args) {
+
+    val allSourceFiles = git.modifiedFiles + git.createdFiles
+    val sourceChanges = allSourceFiles.firstOrNull { it.contains("src") }
+
+    onGitHub {
+        // Big PR Check
+        if ((pullRequest.additions ?: 0) + (pullRequest.deletions ?: 0) > 500) {
+            warn("Big PR, try to keep changes smaller if you can")
+        }
+
+        // Work in progress check
+        if (pullRequest.title.contains("WIP", false)) {
+            warn("PR is classed as Work in Progress")
+        }
+    }
+}
