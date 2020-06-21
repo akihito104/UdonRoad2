ac5d1f025a60f818887b64b233ede22ed2f24fc3
Matsuda, Akihit <matsuda104@gmail.com>
2020-06-21 18:43:29 +0900

    replaced GlobalScope.launch

--- data/repository/src/main/java/com/freshdigitable/udonroad2/data/impl/RelationshipRepository.kt
+++ data/repository/src/main/java/com/freshdigitable/udonroad2/data/impl/RelationshipRepository.kt
@@ -9,15 +9,12 @@ import com.freshdigitable.udonroad2.model.Relationship
 import com.freshdigitable.udonroad2.model.RepositoryScope
 import dagger.Module
 import dagger.Provides
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class RelationshipRepository @Inject constructor(
     private val dao: RelationshipDao,
-    private val restClient: FriendshipRestClient
+    private val restClient: FriendshipRestClient,
+    private val executor: AppExecutor
 ) {
     fun findRelationship(targetUserId: Long): LiveData<Relationship?> {
         val res = MediatorLiveData<Relationship?>()
@@ -31,66 +28,56 @@ class RelationshipRepository @Inject constructor(
     }
 
     private fun fetchFriendship(targetUserId: Long) {
-        GlobalScope.launch {
+        executor.launchIO {
             val f = restClient.fetchFriendship(targetUserId)
             dao.addRelationship(f)
         }
     }
 
     fun updateFollowingStatus(targetUserId: Long, isFollowing: Boolean) {
-        GlobalScope.launch {
+        executor.launchIO {
             val user = if (isFollowing) {
                 restClient.createFriendship(targetUserId)
             } else {
                 restClient.destroyFriendship(targetUserId)
             }
-            withContext(Dispatchers.IO) {
-                dao.updateFollowingStatus(user.id, isFollowing)
-            }
+            dao.updateFollowingStatus(user.id, isFollowing)
         }
     }
 
     fun updateMutingStatus(targetUserId: Long, isMuting: Boolean) {
-        GlobalScope.launch {
-            val user = if (isMuting) {
-                restClient.createMute(targetUserId)
-            } else {
-                restClient.destroyMute(targetUserId)
+        executor.launchIO {
+            val user = when {
+                isMuting -> restClient.createMute(targetUserId)
+                else -> restClient.destroyMute(targetUserId)
             }
-            withContext(Dispatchers.IO) {
-                dao.updateMutingStatus(user.id, isMuting)
-            }
+            dao.updateMutingStatus(user.id, isMuting)
         }
     }
 
     fun updateBlockingStatus(targetUserId: Long, isBlocking: Boolean) {
-        GlobalScope.launch {
-            val user = if (isBlocking) {
-                restClient.createBlock(targetUserId)
-            } else {
-                restClient.destroyBlock(targetUserId)
+        executor.launchIO {
+            val user = when {
+                isBlocking -> restClient.createBlock(targetUserId)
+                else -> restClient.destroyBlock(targetUserId)
             }
-            withContext(Dispatchers.IO) {
-                dao.updateBlockingStatus(user.id, isBlocking)
-            }
+            dao.updateBlockingStatus(user.id, isBlocking)
         }
     }
 
     fun updateWantRetweetStatus(currentRelation: Relationship, wantRetweets: Boolean) {
-        GlobalScope.launch {
+        executor.launchIO {
             val updated = restClient.updateFriendship(
                 currentRelation.userId,
                 currentRelation.notificationsEnabled,
                 wantRetweets
             )
-            withContext(Dispatchers.IO) {
-                dao.updateWantRetweetsStatus(updated.userId, updated.wantRetweets)
-            }
+            dao.updateWantRetweetsStatus(updated.userId, updated.wantRetweets)
         }
     }
 
     fun reportSpam(userId: Long) {
-        GlobalScope.launch {
+        executor.launchIO {
             val user = restClient.reportSpam(userId)
             dao.updateBlockingStatus(user.id, false)
         }
@@ -107,8 +94,9 @@ object RelationshipRepositoryModule {
     @Provides
     fun provideRelationshipRepository(
         dao: RelationshipDao,
-        restClient: FriendshipRestClient
+        restClient: FriendshipRestClient,
+        executor: AppExecutor
     ): RelationshipRepository {
-        return RelationshipRepository(dao, restClient)
+        return RelationshipRepository(dao, restClient, executor)
     }
 }
