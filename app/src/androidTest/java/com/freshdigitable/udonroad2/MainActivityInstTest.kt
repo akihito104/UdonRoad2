993bb34f3d5ec04d0dd7449bc2ea2ac7c184b6c6
Matsuda, Akihit <matsuda104@gmail.com>
2020-07-12 21:08:01 +0900

    added oauth test case

--- app/src/androidTest/java/com/freshdigitable/udonroad2/MainActivityInstTest.kt
+++ app/src/androidTest/java/com/freshdigitable/udonroad2/MainActivityInstTest.kt
@@ -20,7 +20,10 @@ import android.app.Instrumentation
 import android.net.Uri
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.IdlingResource
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents.intending
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasData
@@ -29,12 +32,18 @@ import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.freshdigitable.udonroad2.main.MainActivity
+import io.mockk.confirmVerified
 import io.mockk.every
 import io.mockk.just
+import io.mockk.mockk
 import io.mockk.runs
+import io.mockk.verify
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import twitter4j.ResponseList
+import twitter4j.Status
+import twitter4j.auth.AccessToken
 import twitter4j.auth.RequestToken
 
 @RunWith(AndroidJUnit4::class)
@@ -46,5 +55,68 @@ class MainActivityInstTest {
     fun testLaunch() {
         intentsTestRule.launchActivity(null)
         onView(withId(R.id.oauth_start)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testStartOauth() {
+        intentsTestRule.launchActivity(null)
+        onView(withId(R.id.oauth_start)).check(matches(isDisplayed()))
+
+        val twitter =
+            ApplicationProvider.getApplicationContext<InstTestApplication>().component.twitter
+        every { twitter.setOAuthAccessToken(null) } just runs
+        every {
+            twitter.setOAuthAccessToken(
+                match { it.userId == 10000L }
+            )
+        } just runs
+        every { twitter.getOAuthRequestToken("oob") } returns mockk<RequestToken>().apply {
+            every { token } returns "10000-verified_token"
+            every { tokenSecret } returns "verified_secret_token"
+            every { authorizationURL } returns "http://localhost/hoge"
+        }
+        every {
+            twitter.getOAuthAccessToken(
+                any<RequestToken>(),
+                "0123456"
+            )
+        } returns AccessToken("10000-verified_token", "verified_secret_token")
+        var doneLogin = false
+        intending(hasData(Uri.parse("http://localhost/hoge"))).respondWithFunction {
+            doneLogin = true
+            Instrumentation.ActivityResult(0, null)
+        }
+        every { twitter.homeTimeline } returns mockk<ResponseList<Status>>().apply {
+            val listDelegate = emptyList<Status>()
+            every { size } returns listDelegate.size
+            every { iterator() } returns listDelegate.toMutableList().iterator()
+        }
+
+        onView(withId(R.id.oauth_start)).perform(click())
+        IdlingRegistry.getInstance().register(object : IdlingResource {
+            override fun getName(): String = "idleres"
+
+            override fun isIdleNow(): Boolean {
+                if (doneLogin) {
+                    callback?.onTransitionToIdle()
+                }
+                return doneLogin
+            }
+
+            var callback: IdlingResource.ResourceCallback? = null
+            override fun registerIdleTransitionCallback(callback: IdlingResource.ResourceCallback?) {
+                this.callback = callback
+            }
+        })
+        onView(withId(R.id.oauth_pin)).perform(typeText("0123456"))
+        onView(withId(R.id.oauth_send_pin)).perform(click())
+
+        verify { twitter.setOAuthAccessToken(null) }
+        verify { twitter.setOAuthAccessToken(match { it.userId == 10000L }) }
+        verify { twitter.getOAuthRequestToken("oob") }
+        verify { twitter.getOAuthAccessToken(any<RequestToken>(), "0123456") }
+        verify { twitter.homeTimeline }
+
+        confirmVerified(twitter)
     }
 }
