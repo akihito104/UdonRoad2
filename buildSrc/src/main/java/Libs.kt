7f87b9bcecc3fe1dab7e1e5e496a92b67e433ba8
Matsuda, Akihit <matsuda104@gmail.com>
2020-09-03 21:22:34 +0900

    added buildSrc

---
+++ buildSrc/src/main/java/Libs.kt
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2020. Matsuda, Akihit (akihito104)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+object Libs {
+    const val androidx_appcompat = "androidx.appcompat:appcompat:${Versions.appcompat}"
+    const val androidx_test_arch_core =
+        "androidx.arch.core:core-testing:${Versions.androidx_arch_core_testing}"
+    const val androidx_constraintlayout =
+        "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
+    const val androidx_core_ktx = "androidx.core:core-ktx:${Versions.coreKtx}"
+    const val androidx_fragment_ktx = "androidx.fragment:fragment-ktx:${Versions.fragment}"
+    const val androidx_lifecycle_ext =
+        "androidx.lifecycle:lifecycle-extensions:${Versions.lifecycle}"
+    const val androidx_lifecycle_livedata_ktx =
+        "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.lifecycle}"
+    const val androidx_lifecycle_livedata =
+        "androidx.lifecycle:lifecycle-livedata:${Versions.lifecycle}"
+    const val androidx_lifecycle_rx_ktx =
+        "androidx.lifecycle:lifecycle-reactivestreams-ktx:${Versions.lifecycle}"
+    const val androidx_lifecycle_rx =
+        "androidx.lifecycle:lifecycle-reactivestreams:${Versions.lifecycle}"
+    const val androidx_lifecycle_viewmodel_ktx =
+        "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycle}"
+    const val androidx_lifecycle_viewmodel =
+        "androidx.lifecycle:lifecycle-viewmodel:${Versions.lifecycle}"
+    const val androidx_navigation_fragment_ktx =
+        "androidx.navigation:navigation-fragment-ktx:${Versions.nav}"
+    const val androidx_navigation_ui_ktx = "androidx.navigation:navigation-ui-ktx:${Versions.nav}"
+    const val androidx_paging = "androidx.paging:paging-runtime:${Versions.paging}"
+    const val androidx_recyclerview = "androidx.recyclerview:recyclerview:${Versions.recyclerview}"
+    const val androidx_room_compiler = "androidx.room:room-compiler:${Versions.room}"
+    const val androidx_room_ktx = "androidx.room:room-ktx:${Versions.room}"
+    const val androidx_room_runtime = "androidx.room:room-runtime:${Versions.room}"
+    const val androidx_swiperefreshlayout = "androidx.swiperefreshlayout:swiperefreshlayout:1.0.0"
+    const val androidx_test_core = "androidx.test:core:${Versions.androidx_test_core}"
+    const val androidx_test_espresso = "androidx.test.espresso:espresso-core:${Versions.espresso}"
+    const val androidx_test_espresso_intents =
+        "androidx.test.espresso:espresso-intents:${Versions.espresso}"
+    const val androidx_test_ext_junit = "androidx.test.ext:junit:${Versions.androidx_junit_ext}"
+    const val androidx_test_ext_truth = "androidx.test.ext:truth:${Versions.androidx_truth_ext}"
+    const val androidx_test_rules = "androidx.test:rules:${Versions.androidx_test_rules}"
+    const val androidx_test_runner = "androidx.test:runner:${Versions.androidTestRunner}"
+    const val androidx_viewpager = "androidx.viewpager2:viewpager2:${Versions.viewpager2}"
+    const val glide_compiler = "com.github.bumptech.glide:compiler:${Versions.glide}"
+    const val glide = "com.github.bumptech.glide:glide:${Versions.glide}"
+    const val material = "com.google.android.material:material:${Versions.material}"
+    const val dagger_android = "com.google.dagger:dagger-android:${Versions.dagger}"
+    const val dagger_android_processor =
+        "com.google.dagger:dagger-android-processor:${Versions.dagger}"
+    const val dagger_android_support = "com.google.dagger:dagger-android-support:${Versions.dagger}"
+    const val dagger_compiler = "com.google.dagger:dagger-compiler:${Versions.dagger}"
+    const val truth = "com.google.truth:truth:${Versions.truth}"
+    const val threetenbp = "com.jakewharton.threetenabp:threetenabp:${Versions.threetenbp}"
+    const val timber = "com.jakewharton.timber:timber:${Versions.timber}"
+    const val leakcanary = "com.squareup.leakcanary:leakcanary-android:${Versions.leakcanary}"
+    const val mockk_android = "io.mockk:mockk-android:${Versions.mockk}"
+    const val mockk = "io.mockk:mockk:${Versions.mockk}"
+    const val rxjava2_android = "io.reactivex.rxjava2:rxandroid:${Versions.rxAndroid}"
+    const val rxjava2 = "io.reactivex.rxjava2:rxjava:${Versions.rx}"
+    const val junit = "junit:junit:${Versions.junit}"
+    const val kotlin_stdlib_jdk7 = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
+    const val kotlin_stdlib = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}"
+    const val kotlinx_coroutines_android =
+        "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutine}"
+    const val kotlinx_coroutines =
+        "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutine}"
+    const val kotlinx_test_coroutines =
+        "org.jetbrains.kotlinx:kotlinx-coroutines-test:${Versions.kotlin_coroutine_testing}"
+    const val robolectric = "org.robolectric:robolectric:${Versions.robolectric}"
+    const val twitter4j = "org.twitter4j:twitter4j-core:${Versions.t4j}"
+    const val twitter4j_http_support = "org.twitter4j:twitter4j-http2-support:${Versions.t4j}"
+    const val androidx_test_orchestrator =
+        "androidx.test:orchestrator:${Versions.test_orchestrator}"
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2020. Matsuda, Akihit (akihito104)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+object Versions {
+    const val kotlin = "1.4.0"
+    const val coroutine = "1.3.3"
+
+    const val appcompat = "1.1.0"
+    const val fragment = "1.2.4"
+
+    const val coreKtx = "1.3.0"
+    const val lifecycle = "2.2.0"
+    const val paging = "2.1.0"
+    const val room = "2.2.5"
+    const val nav = "2.3.0"
+
+    const val recyclerview = "1.0.0"
+    const val material = "1.2.0-alpha06"
+    const val constraintLayout = "2.0.0-beta6"
+    const val viewpager2 = "1.0.0-beta03"
+
+    const val dagger = "2.27"
+
+    const val rx = "2.2.19"
+    const val rxAndroid = "2.1.1"
+
+    const val t4j = "4.0.7"
+    const val glide = "4.11.0"
+    const val threetenbp = "1.1.0"
+    const val timber = "4.7.1"
+
+    const val leakcanary = "2.4"
+
+    // test
+    const val junit = "4.12"
+    const val mockk = "1.10.0"
+    const val robolectric = "4.3.1"
+    const val truth = "0.42"
+    const val androidx_test_core = "1.2.0"
+    const val androidx_junit_ext = "1.1.1"
+    const val androidx_test_rules = "1.2.0"
+    const val androidx_truth_ext = "1.2.0"
+    const val androidx_arch_core_testing = "2.1.0"
+    const val kotlin_coroutine_testing = "1.3.9"
+
+    // android test
+    const val androidTestRunner = "1.2.0"
+    const val espresso = "3.2.0"
+    const val test_orchestrator = "1.2.0"
+
+    // ktlint
+    const val ktlint_plugin = "9.2.1"
+    const val ktlint = "0.37.1"
+}
