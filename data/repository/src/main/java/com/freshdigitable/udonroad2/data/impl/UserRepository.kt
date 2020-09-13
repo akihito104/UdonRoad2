0d9d45984883d911dfe8d1fe3d4669594e0c7180
Matsuda, Akihit <matsuda104@gmail.com>
2020-09-13 23:32:21 +0900

    added repository fallback logic

--- data/repository/src/main/java/com/freshdigitable/udonroad2/data/impl/UserRepository.kt
+++ data/repository/src/main/java/com/freshdigitable/udonroad2/data/impl/UserRepository.kt
@@ -1,17 +1,37 @@
 package com.freshdigitable.udonroad2.data.impl
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import com.freshdigitable.udonroad2.data.db.DaoModule
 import com.freshdigitable.udonroad2.data.db.dao.UserDao
+import com.freshdigitable.udonroad2.data.restclient.UserRestClient
 import com.freshdigitable.udonroad2.model.user.User
 import com.freshdigitable.udonroad2.model.user.UserId
 import dagger.Module
 import dagger.Provides
 
 class UserRepository(
-    private val dao: UserDao
+    private val dao: UserDao,
+    private val restClient: UserRestClient,
+    private val appExecutor: AppExecutor,
 ) {
-    fun getUser(id: UserId): LiveData<User?> = dao.getUserById(id)
+    fun getUser(id: UserId): LiveData<User?> {
+        return MediatorLiveData<User?>().apply {
+            addSource(dao.getUserById(id)) { u ->
+                when {
+                    u != null -> this.value = u
+                    else -> fetchUser(id)
+                }
+            }
+        }
+    }
+
+    private fun fetchUser(id: UserId) {
+        appExecutor.launchIO {
+            val user = restClient.showUser(id)
+            dao.addUsers(listOf(user))
+        }
+    }
 }
 
 @Module(
@@ -21,7 +41,11 @@ class UserRepository(
 )
 object UserRepositoryModule {
     @Provides
-    fun provideUserRepository(dao: UserDao): UserRepository {
-        return UserRepository(dao)
+    fun provideUserRepository(
+        dao: UserDao,
+        restClient: UserRestClient,
+        appExecutor: AppExecutor
+    ): UserRepository {
+        return UserRepository(dao, restClient, appExecutor)
     }
 }
---
+++ data/restclient/src/main/java/com/freshdigitable/udonroad2/data/restclient/UserRestClient.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2020. Matsuda, Akihit (akihito104)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.freshdigitable.udonroad2.data.restclient
+
+import com.freshdigitable.udonroad2.data.restclient.ext.toEntity
+import com.freshdigitable.udonroad2.model.user.User
+import com.freshdigitable.udonroad2.model.user.UserId
+import javax.inject.Inject
+
+class UserRestClient @Inject constructor(
+    private val twitter: AppTwitter
+) {
+    suspend fun showUser(id: UserId): User {
+        val u = twitter.fetch { showUser(id.value) }
+        return u.toEntity()
+    }
+}
