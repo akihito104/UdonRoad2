4b42c5ec84f1a2976857c63b4915cbfa6bafbde7
Matsuda, Akihit <matsuda104@gmail.com>
2020-09-19 23:41:58 +0900

    separate some class files for app event

--- model/src/main/java/com/freshdigitable/udonroad2/model/app/navigation/EventDispatcher.kt
+++ model/src/main/java/com/freshdigitable/udonroad2/model/app/navigation/EventDispatcher.kt
@@ -1,29 +1,18 @@
 package com.freshdigitable.udonroad2.model.app.navigation
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.distinctUntilChanged
-import androidx.lifecycle.toLiveData
 import com.freshdigitable.udonroad2.model.app.di.ActivityScope
-import io.reactivex.BackpressureStrategy
-import io.reactivex.Observable
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
-import io.reactivex.subjects.BehaviorSubject
 import io.reactivex.subjects.PublishSubject
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.rx2.rxObservable
 import timber.log.Timber
-import java.io.Serializable
 import javax.inject.Inject
-import kotlin.coroutines.CoroutineContext
-import kotlin.coroutines.EmptyCoroutineContext
 
 @ActivityScope
 class EventDispatcher @Inject constructor() {
     val emitter = PublishSubject.create<AppEvent>()
 
     fun postEvent(event: AppEvent) {
-        Timber.tag("NavigationDispatcher").d("postEvent: $event")
+        Timber.tag("EventDispatcher").d("postEvent: $event")
         emitter.onNext(event)
     }
 }
@@ -32,63 +21,11 @@ fun EventDispatcher.postEvents(vararg events: AppEvent) {
     events.forEach(this::postEvent)
 }
 
-typealias AppAction<T> = Observable<T>
-typealias AppViewState<T> = LiveData<T>
-
 inline fun <reified T> EventDispatcher.toAction(
     block: PublishSubject<AppEvent>.() -> AppAction<T> = { filterByType() }
 ): AppAction<T> {
     return PublishSubject.create<T>().also { action ->
         this.emitter.block().subscribe(action)
-    }
-}
-
-inline fun <T, reified E> AppAction<T>.toViewState(
-    block: AppAction<T>.() -> Observable<E> = { cast(E::class.java) }
-): AppViewState<E> {
-    return BehaviorSubject.create<E>().also { action ->
-        this.block().subscribe(action)
-    }
-        .toFlowable(BackpressureStrategy.BUFFER)
-        .toLiveData()
-        .distinctUntilChanged()
-}
-
-inline fun <reified T> AppAction<out AppEvent>.filterByType(): AppAction<T> {
-    return this.filter { it is T }.cast(T::class.java)
-}
-
-@ExperimentalCoroutinesApi
-inline fun <E : AppEvent, R> AppAction<E>.suspendMap(
-    coroutineContext: CoroutineContext = EmptyCoroutineContext,
-    crossinline block: suspend (E) -> R
-): AppAction<EventResult<E, R>> = flatMap { event ->
-    rxObservable(coroutineContext) {
-        val result = runCatching { block(event) }
-        channel.send(EventResult(event, result))
-    }
-}
-
-data class EventResult<E : AppEvent, T>(
-    val event: E,
-    private val result: Result<T>
-) : Serializable {
-    val value: T? = result.getOrNull()
-    val isSuccess: Boolean
-        get() = result.isSuccess
-    val isFailure: Boolean
-        get() = result.isFailure
-    val exception: Throwable?
-        get() = result.exceptionOrNull()
-
-    companion object {
-        fun <E : AppEvent, T> success(event: E, value: T): EventResult<E, T> {
-            return EventResult(event, Result.success(value))
-        }
-
-        fun <E : AppEvent, T> failure(event: E, throwable: Throwable): EventResult<E, T> {
-            return EventResult(event, Result.failure(throwable))
-        }
     }
 }
 
---
+++ model/src/main/java/com/freshdigitable/udonroad2/model/app/navigation/EventResult.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2020. Matsuda, Akihit (akihito104)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.freshdigitable.udonroad2.model.app.navigation
+
+import java.io.Serializable
+
+data class EventResult<E : AppEvent, T>(
+    val event: E,
+    private val result: Result<T>
+) : Serializable {
+    val value: T? = result.getOrNull()
+    val isSuccess: Boolean
+        get() = result.isSuccess
+    val isFailure: Boolean
+        get() = result.isFailure
+    val exception: Throwable?
+        get() = result.exceptionOrNull()
+
+    companion object {
+        fun <E : AppEvent, T> success(event: E, value: T): EventResult<E, T> {
+            return EventResult(event, Result.success(value))
+        }
+
+        fun <E : AppEvent, T> failure(event: E, throwable: Throwable): EventResult<E, T> {
+            return EventResult(event, Result.failure(throwable))
+        }
+    }
+}
