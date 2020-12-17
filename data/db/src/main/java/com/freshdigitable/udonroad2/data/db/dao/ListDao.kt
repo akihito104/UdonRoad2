34e420528b8dceec6f70c9720b467c45bc9c220c
Matsuda, Akihit <matsuda104@gmail.com>
2020-12-17 14:10:48 +0900

    changed to avoid nullable arguments

--- data/db/src/main/java/com/freshdigitable/udonroad2/data/db/dao/ListDao.kt
+++ data/db/src/main/java/com/freshdigitable/udonroad2/data/db/dao/ListDao.kt
@@ -24,8 +24,8 @@ class TweetListDao(
 
     override suspend fun putList(
         entities: List<TweetEntity>,
-        query: ListQuery<QueryType.TweetQueryType>?,
-        owner: ListId?
+        query: ListQuery<QueryType.TweetQueryType>,
+        owner: ListId
     ) {
         dao.addTweets(entities, owner)
     }
@@ -45,8 +45,8 @@ class UserListDao(
 
     override suspend fun putList(
         entities: List<UserEntity>,
-        query: ListQuery<QueryType.UserQueryType>?,
-        owner: ListId?
+        query: ListQuery<QueryType.UserQueryType>,
+        owner: ListId
     ) {
         dao.addUsers(entities.map { it.toEntity() }, owner)
     }
@@ -66,8 +66,8 @@ class CustomTimelineListDao(
 
     override suspend fun putList(
         entities: List<CustomTimelineEntity>,
-        query: ListQuery<QueryType.UserListMembership>?,
-        owner: ListId?
+        query: ListQuery<QueryType.UserListMembership>,
+        owner: ListId
     ) {
         dao.addCustomTimeline(entities, owner)
     }
