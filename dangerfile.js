37377fb49aea7cd62fe1e73fa4b00ced1dd7a2f2
Matsuda, Akihit <matsuda104@gmail.com>
2021-08-28 20:27:04 +0900

    replaced danger/kotlin with danger/js

---
+++ dangerfile.js
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2021. Matsuda, Akihit (akihito104)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { danger, fail, markdown, message, peril, schedule, warn } from 'danger'
+
+const changedLine = danger.github.pr.additions + danger.github.deletions;
+if (changedLine > 500) {
+  warn("Big PR, try to keep changes smaller if you can");
+}
+
+if (danger.github.pr.title.contains("WIP", false)) {
+  warn("PR is classed as Work in Progress");
+}
